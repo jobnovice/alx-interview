64e91749ccf5b6d564af1c89a4f66eaa92691e53
jobnovice <jobkebede47@gmail.com>
2024-10-23 00:24:37 +0300

    SILO 2

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Blaire pascal's number immplemented"""
+
+def pascal_triangle(n):
+    """pascal_triangle implemented using python"""
+    new_list = []
+    if n <= 0:
+        return new_list
+    for i in range(n):
+        row = []
+        for j in range(n):
+            if i == j or j == 0:
+                row.append(1)
+            elif i > j:
+                row.append(new_list[i-1][j-1] + new_list[i-1][j])
+        new_list.append(row)
+    return new_list                 
